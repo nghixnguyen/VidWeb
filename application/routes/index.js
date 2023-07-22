f7c94228d58e4d1276242ac3d9803100fcc0e0dd
nghi_nguyen <nghinshopping@gmail.com>
2023-07-21 20:23:59 -0700

    change views folder for M2

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
+router.get("/index", function(req, res, next) {
+  res.render('index');
 });
+
+router.get("/login", function(req, res, next) {
+  res.render('login', {title: "Login", css:["style.css"]});
+});
+
+router.get("/postvideo", function(req, res, next) {
+  res.render('postvideo');
+});
+
+router.get("/registration", function(req, res, next) {
+  res.render('registration', {title: "Registration", js:["validation.js"]});
+});
+
+router.get("/viewpost", function(req, res, next) {
+  // router.get("/viewpost/:id(\\d+)", function(req, res, next) {
+  res.render('viewpost');
+});
+
 
 module.exports = router;
